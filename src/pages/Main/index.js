2721fb7a71fc56831b5d41b5c78a768b4ea1df72
robersonfox <roberson@pmmg.mg.gov.br>
2019-08-01 17:46:57 -0300

    Envio para o Git Hub

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container } from './styles'
- 
+import { Header } from '~/components/header'
 
 export default function Main() {
   return (
-    <Container />
+    <Container>
+      <Header></Header>
+    </Container>
   )
 };
